c3860d2ff971097aac3bdadc00565f22e2ea411c
Fiona <mlee353@wisc.edu>
2020-11-12 00:41:35 -0600

    P4: add, delete done. Edit: indicate current time.

--- ExercisesView.js
+++ ExercisesView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, Dimensions, Modal } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, Dimensions, Modal, FlatList, List, SafeAreaView } from 'react-native';
 
 class ExercisesView extends React.Component {
     constructor(props) {
@@ -12,15 +12,58 @@ class ExercisesView extends React.Component {
             date: "",
             exercises: [],
             add: false,
+            edit: false, 
         }
         this.getAllExercises = this.getAllExercises.bind(this);
         this.addExercise = this.addExercise.bind(this);
+        this.deleteExercise = this.deleteExercise.bind(this);
     }
 
     addExercise() {
         fetch('https://mysqlcs639.cs.wisc.edu/activities', {
             method: 'POST',
-            headers: { 'x-access-token': this.props.accessToken },
+            headers: { 
+                'Content-Type': 'application/json',
+                'x-access-token': this.props.accessToken 
+            },
+            body: JSON.stringify({
+                name: this.state.name,
+                duration: this.state.duration,
+                calories: this.state.calories,
+                date: this.state.date
+            })
+        })
+      .then(res => res.json())
+      .then(res => {
+        if(res) {
+            alert(JSON.stringify(res.message));
+        }
+      });
+    }
+
+    deleteExercise(id) {
+        fetch(`https://mysqlcs639.cs.wisc.edu/activities/` + id, {
+        method: 'DELETE',
+        headers: {
+            'x-access-token': this.props.accessToken
+        },
+    })
+      .then(res => res.json())
+      .then(res => {
+        alert("This exercise has been deleted!");
+      })
+      .catch(err => {
+        alert("Something went wrong!");
+      })
+    }
+
+    editExercise(id) {
+        fetch(`https://mysqlcs639.cs.wisc.edu/activities/` + id, {
+            method: 'PUT',
+            headers: { 
+                'Content-Type': 'application/json',
+                'x-access-token': this.props.accessToken 
+            },
             body: JSON.stringify({
                 name: this.state.name,
                 duration: this.state.duration,
@@ -41,25 +84,7 @@ class ExercisesView extends React.Component {
     }
 
     getAllExercises() {
-        return(
-        <>
-        {this.state.exercises.map((exe, id) => (
-            <Card>
-            <Card.Header as="h5">{exe.name}</Card.Header>
-            <Card.Body>
-                <Card.Text>Date: {exe.date}</Card.Text>
-                <Card.Text>Duration: {exe.duration}</Card.Text>
-                <Card.Text>Calories burned: {exe.calories}</Card.Text>
-                <Button variant="primary">Edit</Button>
-            </Card.Body>
-        </Card>
-        ))}
-        </>
-        )
-    }
-
-    componentDidMount() {
-    fetch('https://mysqlcs639.cs.wisc.edu/activities', {
+        fetch('https://mysqlcs639.cs.wisc.edu/activities', {
             method: 'GET',
             headers: { 'x-access-token': this.props.accessToken }
         })
@@ -69,6 +94,42 @@ class ExercisesView extends React.Component {
           exercises: res.activities
         });
       });
+
+        return (
+            <>
+            {this.state.exercises.map((item) => 
+                <View style={styles.item} key={item.id}>
+                    <Text style={styles.title}>{item.name}</Text>
+                    <View style={styles.spaceSmall}></View>
+                    <Text>Date: {item.date}</Text>
+                    <Text>Duration: {item.duration}</Text>
+                    <Text>Calories burned: {item.calories}</Text>
+                    <Button title="Edit" onPress={() => {
+                        this.setState({ 
+                            id: item.id,
+                            name: item.name,
+                            date: item.date,
+                            duration: item.duration, 
+                            calories: item.calories,
+                            edit: true,
+                        });
+                    }}/>
+                    <Button title="Remove" onPress={() => {
+                        this.setState({id: item.id});
+                        this.deleteExercise(item.id);
+                    }}/>
+                    
+                </View>
+            )}
+            
+            </>
+        )
+    }
+
+    
+
+    componentDidMount() {
+        
     }
 
     render() {
@@ -78,9 +139,7 @@ class ExercisesView extends React.Component {
                     <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
                     <Text style={styles.bigText}>Exercises</Text>
                     </View>
-                    <View style={styles.spaceSmall}></View>
-                    <Text>Let's get to know you!</Text>
-                    <Text>Specify your information below.</Text>
+                    
                 <View style={styles.space} />
                 {this.getAllExercises()}
                 <Button color="#a1635f" style={styles.buttonInline} title="Add" onPress={() => this.setState({add: true})} />
@@ -90,136 +149,70 @@ class ExercisesView extends React.Component {
                     <View style={styles.modalView}>
                     <Text>Activity name:</Text> 
                     <TextInput style={styles.input}
-                        underlineColorAndroid="transparent"
                         placeholderTextColor="#d9bebd"
                         onChangeText={(name) => this.setState({ name: name })}
                         value={this.state.name}
                         autoCapitalize="none" />
                         <Text>Duration: </Text>
                         <TextInput style={styles.input}
-                            underlineColorAndroid="transparent"
                             placeholderTextColor="#d9bebd"
                             onChangeText={(duration) => this.setState({ duration: duration })}
                             value={this.state.duration}
                             autoCapitalize="none" />
                         <Text>Calories burned: </Text>
                         <TextInput style={styles.input}
-                            underlineColorAndroid="transparent"
                             placeholderTextColor="#d9bebd"
                             onChangeText={(calories) => this.setState({ calories: calories })}
                             value={this.state.calories}
                             autoCapitalize="none" />
                         <Text>Date: </Text>
                         <TextInput style={styles.input}
-                            underlineColorAndroid="transparent"
                             placeholderTextColor="#d9bebd"
                             onChangeText={(date) => this.setState({ date: date })}
                             value={this.state.date}
                             autoCapitalize="none" />
 
                         <Button title="Add" onPress={() => {
-                            this.setState({add: false});
                             this.addExercise();
+                            this.setState({add: false});
                         }}/>
                         <Button title="Close" onPress={() => this.setState({add: false})}/>
                     </View>
                     </View>
                 </Modal>
-
-                <Text style={{ textAlignVertical: "center", fontWeight: "700", fontSize: 20 }}>Personal Information</Text>
-        <View style={styles.spaceSmall}></View>
-        <View>
-          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>First Name</Text>
-        </View>
-        <TextInput style={styles.input}
-          underlineColorAndroid="transparent"
-          placeholder="Bucky"
-          placeholderTextColor="#d9bebd"
-          onChangeText={(firstName) => this.setState({ firstName: firstName })}
-          value={this.state.firstName}
-          autoCapitalize="none" />
-        <View style={styles.spaceSmall}></View>
-
-        <View>
-          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Last Name</Text>
-        </View>
-        <TextInput style={styles.input}
-          underlineColorAndroid="transparent"
-          placeholder="Badger"
-          placeholderTextColor="#d9bebd"
-          onChangeText={(lastName) => this.setState({ lastName: lastName })}
-          value={this.state.lastName}
-          autoCapitalize="none" />
-        <View style={styles.spaceSmall}></View>
-
-        <Text style={{ textAlignVertical: "center", fontWeight: "700", fontSize: 20 }}>Fitness Goals</Text>
-        <View style={styles.spaceSmall}></View>
-        <View>
-          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Daily Calories (kcal)</Text>
-        </View>
-        <TextInput style={styles.input}
-          underlineColorAndroid="transparent"
-          placeholder="2200"
-          placeholderTextColor="#d9bebd"
-          onChangeText={(goalDailyCalories) => this.setState({ goalDailyCalories: goalDailyCalories })}
-          value={this.state.goalDailyCalories + ""}
-          autoCapitalize="none" />
-        <View style={styles.spaceSmall}></View>
-        <View>
-          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Daily Protein (grams)</Text>
-        </View>
-        <TextInput style={styles.input}
-          underlineColorAndroid="transparent"
-          placeholder="52"
-          placeholderTextColor="#d9bebd"
-          onChangeText={(goalDailyProtein) => this.setState({ goalDailyProtein: goalDailyProtein })}
-          value={this.state.goalDailyProtein + ""}
-          autoCapitalize="none" />
-        <View style={styles.spaceSmall}></View>
-        <View>
-          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Daily Carbs (grams)</Text>
-        </View>
-        <TextInput style={styles.input}
-          underlineColorAndroid="transparent"
-          placeholder="130"
-          placeholderTextColor="#d9bebd"
-          onChangeText={(goalDailyCarbohydrates) => this.setState({ goalDailyCarbohydrates: goalDailyCarbohydrates })}
-          value={this.state.goalDailyCarbohydrates + ""}
-          autoCapitalize="none" />
-        <View style={styles.spaceSmall}></View>
-        <View>
-          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Daily Fat (grams)</Text>
-        </View>
-        <TextInput style={styles.input}
-          underlineColorAndroid="transparent"
-          placeholder="35"
-          placeholderTextColor="#d9bebd"
-          onChangeText={(goalDailyFat) => this.setState({ goalDailyFat: goalDailyFat })}
-          value={this.state.goalDailyFat + ""}
-          autoCapitalize="none" />
-        <View style={styles.spaceSmall}></View>
-        <View>
-          <Text style={{ textAlignVertical: "center", fontWeight: "700" }}>Daily Activity (mins)</Text>
-        </View>
-        <TextInput style={styles.input}
-          underlineColorAndroid="transparent"
-          placeholder="60"
-          placeholderTextColor="#d9bebd"
-          onChangeText={(goalDailyActivity) => this.setState({ goalDailyActivity: goalDailyActivity })}
-          value={this.state.goalDailyActivity + ""}
-          autoCapitalize="none" />
-        <View style={styles.spaceSmall}></View>
-
-        <View style={styles.space} />
-
-        <Text style={{ fontWeight: "700", fontSize: 20 }}>Looks good! All set?</Text>
-        <View style={styles.spaceSmall} />
-
-        <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
-          <Button color="#942a21" style={styles.buttonInline} title="Save Profile" onPress={() => this.handleSaveProfile()} />
-          <View style={styles.spaceHorizontal} />
-          <Button color="#a1635f" style={styles.buttonInline} title="Exit" onPress={() => this.backToLogin()} />
-        </View>
+                <Modal visible={this.state.edit} >
+                    <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                    <Text>Activity name:</Text> 
+                    <TextInput style={styles.input}
+                        placeholderTextColor="#d9bebd"
+                        placeholder={this.state.name}
+                        onChangeText={(name) => this.setState({ name: name })}
+                        value={this.state.name}
+                        autoCapitalize="none" />
+                        <Text>Duration: </Text>
+                        <TextInput style={styles.input}
+                            placeholderTextColor="#d9bebd"
+                            placeholder={this.state.duration.toString()}
+                            onChangeText={(duration) => this.setState({ duration: duration })}
+                            value={this.state.duration}
+                            autoCapitalize="none" />
+                        <Text>Calories burned: </Text>
+                        <TextInput style={styles.input}
+                            placeholderTextColor="#d9bebd"
+                            placeholder={this.state.calories.toString()}
+                            onChangeText={(calories) => this.setState({ calories: calories })}
+                            value={this.state.calories}
+                            autoCapitalize="none" />
+                        <Text>Date: {this.state.date}</Text>
+                        <Button title="Save" onPress={() => {
+                            this.editExercise(this.state.id);
+                            this.setState({edit: false});
+                        }}/>
+                        <Button title="Close" onPress={() => this.setState({edit: false})}/>
+                    </View>
+                    </View>
+                </Modal>
         <View style={styles.space} />
       </ScrollView>
         )
@@ -296,6 +289,15 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
         elevation: 5
+      },
+      item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+      },
+      title: {
+        fontSize: 32,
       },
 });
 
